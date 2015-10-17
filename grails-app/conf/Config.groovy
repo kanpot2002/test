808a949787b1888fac16575ab64adde2f63d9f3f
Besto <besamato@gmail.com>
2015-10-17 21:13:42 +0700

    Besto : change redirect URL

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -133,5 +133,5 @@ grails.plugin.springsecurity.controllerAnnotations.staticRules = [
 	'/**/images/**':    ['permitAll'],
 	'/**/favicon.ico':  ['permitAll']
 ]
-grails.plugin.springsecurity.successHandler.defaultTargetUrl = '/user'
+grails.plugin.springsecurity.successHandler.defaultTargetUrl = '/index'
 
