6b39dccb38a4c97787ae06a8ca392cb67b603ab8
Besto <besamato@gmail.com>
2015-11-28 00:05:34 +0700

    test 2

--- grails-app/utils/Constant.groovy
+++ grails-app/utils/Constant.groovy
@@ -4,5 +4,5 @@
 class Constant {
     public static int numGuarantee = 2;
     public static int maxGuaranteeBalance = 150000;
-    public static int besto = 111;
+    public static int dale = 555;
 }
